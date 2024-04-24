09b806e38ddaee228ef2572b2f33f686a5bbcfee
MolkovAlexandr <163853240+MolkovAlexandr@users.noreply.github.com>
2024-04-24 20:44:38 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,46 +4,46 @@
 #include"Automata.h"
 
 int main() {
- setlocale(LC_ALL, "1251");
+setlocale(LC_ALL, "1251");
 
- Automata automata;
+Automata automata;
 
- int sum = 0;
- int decision = 0;
- int option = 0;
+int sum = 0;
+int decision = 0;
+int option = 0;
 
- automata.on();
+automata.on();
 
- while (true) {
- std::cout << "Введите зачисляемую сумму: "; std::cin >> sum;
+while (true) {
+std::cout << "Введите зачисляемую сумму: "; std::cin >> sum;
 
- automata.coin(sum);
- automata.getMenu({"Espresso", "Latte", "Americano", "Lungo"},
- {50, 65, 55, 70});
+automata.coin(sum);
+automata.getMenu({"Espresso", "Latte", "Americano", "Lungo"},
+{50, 65, 55, 70});
 
- std::cout << "Введите номер желаемой позиции: "; std::cin >> decision;
+std::cout << "Введите номер желаемой позиции: "; std::cin >> decision;
 
- automata.choice(decision);
- if (automata.check(decision)) {
- automata.cook(decision);
- automata.finish();
- } else {
- std::cout << "Пополнить баланс? (1) да, (2) нет: "; std::cin >> option;
- switch (option) {
- case 1: {
- break;
- }
- case 2: {
- automata.cancel();
- break;
- }
- default: {
- std::cout << "Невозможная операция";
- exit(0);
- break;
- }
- }
-  }
- }
- return 0;
+automata.choice(decision);
+if (automata.check(decision)) {
+automata.cook(decision);
+automata.finish();
+} else {
+std::cout << "Пополнить баланс? (1) да, (2) нет: "; std::cin >> option;
+switch (option) {
+case 1: {
+break;
+}
+case 2: {
+automata.cancel();
+break;
+}
+default: {
+std::cout << "Невозможная операция";
+exit(0);
+break;
+}
+}
+}
+}
+return 0;
 }
