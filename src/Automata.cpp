25126d2cbb71d7cfd79970ab8dad978b47f474dd
MolkovAlexandr <163853240+MolkovAlexandr@users.noreply.github.com>
2024-04-24 16:12:27 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -3,79 +3,66 @@
 #include<iostream>
 #include"Automata.h"
 
-Automata::Automata()
-{
+Automata::Automata() {
 	state = OFF;
 	cash = 0;
 }
 
-void Automata::on()
-{
+void Automata::on() {
 	state = WAIT;
 }
 
-void Automata::off()
-{
+void Automata::off() {
 	state = OFF;
 }
 
-void Automata::coin(int cash)
-{
+void Automata::coin(int cash) {
 	this->cash += cash;
 }
 
-void Automata::getMenu(const std::vector<std::string>&menu, const std::vector<int>&prices)
-{
+void Automata::getMenu(const std::vector<std::string>&menu, const std::vector<int>&prices) {
 	this->menu = menu; this->prices = prices;
 }
 
-States Automata::getState()
-{
+States Automata::getState() {
 	return state;
 }
 
-void Automata::choice(int position)
-{
-	if (position >= 0 && position < menu.size())
-	{
+void Automata::choice(int position) {
+	if (position >= 0 && position < menu.size()){
 		state = CHECK;
 		std::cout << "Выбрана позиция " << menu.at(position) << std::endl;
 	}
 	else std::cout << "Невозможная операция" << std::endl;
 }
 
-bool Automata::check(int position)
-{
-	if (cash >= prices.at(position))
-	{
+bool Automata::check(int position) {
+	if (cash >= prices.at(position)){
 		state = COOK;
 		std::cout << "Приготовление начато" << std::endl;
 		return true;
 	}
-	else 
-	{ 
-		std::cout << "Недостаточно " << prices.at(position) - cash << " для покупки позиции " << menu.at(position) << std::endl; 
+	else { 
+		std::cout << "Недостаточно " << prices.at(position) - cash 
+			<< " для покупки позиции " << menu.at(position) << std::endl; 
 		return false;
 	}
 }
 
-void Automata::cancel()
-{
+void Automata::cancel() {
 	state = WAIT;
 	cash = 0;
 	std::cout << "Операция отменена" << std::endl;
 }
 
-void Automata::cook(int position)
-{
+void Automata::cook(int position) {
 	state = WAIT;
 	cash -= prices.at(position);
 	std::cout << "Напиток готов, сдача: " << cash << std::endl;
 	cash = 0;
 }
 
-void Automata::finish()
-{
+void Automata::finish() {
 	state = WAIT;
 	cash = 0;
 	std::cout << "Операция успешно завершена" << std::endl;
