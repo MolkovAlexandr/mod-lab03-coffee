77f589e81f0b363f883ba4e20eea1c23cd91a5e4
MolkovAlexandr <163853240+MolkovAlexandr@users.noreply.github.com>
2024-04-24 15:46:10 +0300

    Add files via upload

---
+++ include/Automata.h
@@ -0,0 +1,26 @@
+#pragma once
+#include<string>
+#include<vector>
+
+enum States { OFF, WAIT, ACCEPT, CHECK, COOK };
+
+class Automata
+{
+	int cash;
+	std::vector<std::string>menu;
+	std::vector<int>prices;
+	States state;
+
+public:
+	Automata();
+	void on();
+	void off();
+	void coin(int cash);
+	void getMenu(const std::vector<std::string>& menu, const std::vector<int>& prices);
+	States getState();
+	void choice(int position);
+	bool check(int position);
+	void cancel();
+	void cook(int position);
+	void finish();
+};
